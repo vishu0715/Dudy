8928f965b2d89c6b19909ebd9b39c373be7f363f
vishu0715 <133125417+vishu0715@users.noreply.github.com>
2024-09-10 20:40:51 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-
+import path from 'path';
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
@@ -9,6 +9,8 @@ import userRoutes from './routes/user.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 import {app, io, server} from "./socket/socket.js";
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 dotenv.config();
 
 app.use(express.json());
@@ -16,6 +18,10 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 //app.get("/", (req, res) => {
     //root route http://localhost:5000/
   //  res.send("Hello World!, I am running on port 5000");
